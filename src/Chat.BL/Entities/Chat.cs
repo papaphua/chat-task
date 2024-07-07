6ea417233b4f5eabfa1d0f052cc7be6d203f3203
Yevhenii Nabok <1337niceshot@gmail.com>
2024-07-07 18:00:48 +0300

    add entities

---
+++ src/Chat.BL/Entities/Chat.cs
@@ -0,0 +1,26 @@
+﻿namespace Chat.BL.Entities;
+
+public sealed class Chat
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Name { get; set; } = default!;
+
+    public string? Description { get; set; }
+
+    public Guid OwnerId { get; set; }
+
+    public User Owner { get; set; } = default!;
+
+    public ICollection<Membership> Memberships { get; set; } = default!;
+
+    public static Chat Create(string name, Guid ownerId, string? description = default)
+    {
+        return new Chat
+        {
+            Name = name,
+            OwnerId = ownerId,
+            Description = description
+        };
+    }
+}
