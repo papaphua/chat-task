3261227234a457675d1438cddfd3fb3e5d5fc6e8
Yevhenii Nabok <1337niceshot@gmail.com>
2024-07-07 16:01:48 +0300

    init

---
+++ src/Chat.App/HostingExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Chat.Presentation;
+
+namespace Chat.App;
+
+public static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers()
+            .AddApplicationPart(AssemblyReference.Assembly);
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.MapControllers();
+
+        return app;
+    }
+}
---
+++ src/Chat.Presentation/AssemblyReference.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace Chat.Presentation;
+
+public static class AssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+}
