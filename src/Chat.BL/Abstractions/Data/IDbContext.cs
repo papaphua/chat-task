37753614da1b32d7a5569ed989c17bc64e58e909
Yevhenii Nabok <1337niceshot@gmail.com>
2024-07-08 00:18:31 +0300

    fix db context

--- src/Chat.BL/Abstractions/Data/IDbContext.cs
+++ src/Chat.BL/Abstractions/Data/IDbContext.cs
@@ -10,27 +10,12 @@ public interface IDbContext
     Task AddAsync<TEntity>(TEntity entity)
         where TEntity : class;
 
-    Task AddRangeAsync<TEntity>(params TEntity[] entities)
-        where TEntity : class;
-
-    Task AddRangeAsync<TEntity>(IEnumerable<TEntity> entities)
-        where TEntity : class;
-
     void Remove<TEntity>(TEntity entity)
-        where TEntity : class;
-
-    void RemoveRange<TEntity>(params TEntity[] entities)
         where TEntity : class;
 
     void RemoveRange<TEntity>(IEnumerable<TEntity> entities)
         where TEntity : class;
 
     void Update<TEntity>(TEntity entity)
-        where TEntity : class;
-
-    void UpdateRange<TEntity>(params TEntity[] entities)
-        where TEntity : class;
-
-    void UpdateRange<TEntity>(IEnumerable<TEntity> entities)
         where TEntity : class;
 }
--- src/Chat.DAL/ApplicationDbContext.cs
+++ src/Chat.DAL/ApplicationDbContext.cs
@@ -14,32 +14,11 @@ public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> 
             .AddAsync(entity);
     }
 
-    public async Task AddRangeAsync<TEntity>(params TEntity[] entities)
-        where TEntity : class
-    {
-        await Set<TEntity>()
-            .AddRangeAsync(entities);
-    }
-
-    public async Task AddRangeAsync<TEntity>(IEnumerable<TEntity> entities)
-        where TEntity : class
-    {
-        await Set<TEntity>()
-            .AddRangeAsync(entities);
-    }
-
     public new void Remove<TEntity>(TEntity entity)
         where TEntity : class
     {
         Set<TEntity>()
             .Remove(entity);
-    }
-
-    public void RemoveRange<TEntity>(params TEntity[] entities)
-        where TEntity : class
-    {
-        Set<TEntity>()
-            .RemoveRange(entities);
     }
 
     public void RemoveRange<TEntity>(IEnumerable<TEntity> entities)
@@ -54,20 +33,6 @@ public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> 
     {
         Set<TEntity>()
             .Update(entity);
-    }
-
-    public void UpdateRange<TEntity>(params TEntity[] entities)
-        where TEntity : class
-    {
-        Set<TEntity>()
-            .UpdateRange(entities);
-    }
-
-    public void UpdateRange<TEntity>(IEnumerable<TEntity> entities)
-        where TEntity : class
-    {
-        Set<TEntity>()
-            .UpdateRange(entities);
     }
 
     public async Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default)
