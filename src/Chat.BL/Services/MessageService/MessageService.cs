443fb85f47b2ac81213cd88f5e6b78daf48120ab
Yevhenii Nabok <1337niceshot@gmail.com>
2024-07-08 01:01:53 +0300

    implement message service

--- src/Chat.BL/Services/MessageService/MessageService.cs
+++ src/Chat.BL/Services/MessageService/MessageService.cs
@@ -1,5 +1,70 @@
-﻿namespace Chat.BL.Services.MessageService;
+﻿using Chat.BL.Abstractions.Data;
+using Chat.BL.Abstractions.Results;
+using Chat.BL.Entities;
+using Chat.BL.Errors;
+using Chat.BL.Requests;
+using Microsoft.EntityFrameworkCore;
 
-public sealed class MessageService : IMessageService
+namespace Chat.BL.Services.MessageService;
+
+public sealed class MessageService(
+    IDbContext db,
+    IUnityOfWork unityOfWork)
+    : IMessageService
 {
+    public async Task<Result<Message>> GetMessageAsync(Guid userId, Guid messageId)
+    {
+        var message = await db.Set<Message>()
+            .FirstOrDefaultAsync(m => m.Id == messageId && m.SenderId == userId);
+
+        return message is null
+            ? Result<Message>.Failure(MessageError.NotFound)
+            : Result<Message>.Success(message);
+    }
+
+    public async Task<Result<List<Message>>> GetAllMessagesAsync(Guid userId, Guid chatId)
+    {
+        var membership = await db.Set<Membership>()
+            .FirstOrDefaultAsync(m => m.UserId == userId && m.ChatId == chatId);
+
+        if (membership is null) return Result<List<Message>>.Failure(ChatError.NotFound);
+        
+        var messages = await db.Set<Message>()
+            .Where(m => m.ChatId == chatId)
+            .OrderByDescending(m => m.Timestamp)
+            .ToListAsync();
+        
+        return Result<List<Message>>.Success(messages);
+    }
+
+    public async Task<Result<Message>> CreateMessageAsync(Guid userId, Guid chatId, MessageRequest.CreateMessage request)
+    {
+        var membership = await db.Set<Membership>()
+            .FirstOrDefaultAsync(m => m.UserId == userId && m.ChatId == chatId);
+
+        if (membership is null) return Result<Message>.Failure(ChatError.NotFound);
+
+        var message = Message.Create(
+            userId,
+            chatId,
+            request.Text);
+
+        await db.AddAsync(message);
+        await unityOfWork.SaveChangesAsync();
+
+        return Result<Message>.Success(message);
+    }
+
+    public async Task<Result> RemoveMessageAsync(Guid userId, Guid messageId)
+    {
+        var message = await db.Set<Message>()
+            .FirstOrDefaultAsync(m => m.Id == messageId && m.SenderId == userId);
+
+        if (message is null) return Result.Failure(MessageError.NotFound);
+
+        db.Remove(message);
+        await unityOfWork.SaveChangesAsync();
+
+        return Result.Success();
+    }
 }
--- src/Chat.Presentation/Controllers/MessageController.cs
+++ src/Chat.Presentation/Controllers/MessageController.cs
@@ -1,4 +1,7 @@
-﻿using Chat.BL.Services.MessageService;
+﻿using Chat.BL.Requests;
+using Chat.BL.Services.MessageService;
+using Chat.Presentation.Extensions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Chat.Presentation.Controllers;
@@ -7,4 +10,43 @@ namespace Chat.Presentation.Controllers;
 [Route("api/message")]
 public sealed class MessageController(IMessageService messageService)
 {
+    [HttpGet("{userId:guid}/{messageId:guid}")]
+    public async Task<IResult> GetMessage(Guid userId, Guid messageId)
+    {
+        var result = await messageService.GetMessageAsync(userId, messageId);
+
+        return result.IsSuccess
+            ? Results.Ok(result.Value)
+            : result.ToProblemDetails();
+    }
+
+    [HttpGet("{userId:guid}/{chatId:guid}/all")]
+    public async Task<IResult> GetAllMessages(Guid userId, Guid chatId)
+    {
+        var result = await messageService.GetAllMessagesAsync(userId, chatId);
+
+        return result.IsSuccess
+            ? Results.Ok(result.Value)
+            : result.ToProblemDetails();
+    }
+
+    [HttpPost("{userId:guid}/{chatId:guid}")]
+    public async Task<IResult> CreateMessage(Guid userId, Guid chatId, MessageRequest.CreateMessage request)
+    {
+        var result = await messageService.CreateMessageAsync(userId, chatId, request);
+
+        return result.IsSuccess
+            ? Results.Ok(result.Value)
+            : result.ToProblemDetails();
+    }
+
+    [HttpDelete("{userId:guid}/{messageId:guid}")]
+    public async Task<IResult> RemoveMessage(Guid userId, Guid messageId)
+    {
+        var result = await messageService.RemoveMessageAsync(userId, messageId);
+
+        return result.IsSuccess
+            ? Results.Ok()
+            : result.ToProblemDetails();
+    }
 }
